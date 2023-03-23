92f1ebf0e120fc2207d2e59292c572b61c23573e
Yael Davidov <yael.davidov97@gmail.com>
2023-03-23 16:14:56 +0200

    Create position classes
    - Create position class and position's subclasses in utils/position.js file.
    - Fix the state of "ConnectionPopup" component in the network tab component.

--- client-app/src/testData/position.js
+++ client-app/src/testData/position.js
@@ -1,94 +1,34 @@
-
-let positionId = 0;
-let positionQuestionsId = 1;
-let interviewId = 1;
-let connectionId = 1;
-let homeAssignmentsId = 1;
-
-export function incrementConnectionId(){
-    return connectionId++;
-}
-
-export function createNetworkConnections(name, role, linkToLinkedin, applied){
-    const id = connectionId++;
-    const appliedMe = applied ?? false;
-    return {name, role, linkToLinkedin, appliedMe, id};
-}
+import { HomeAssignment, Interview, NetworkConnection, Position, PositionInfo, QuestionsAndAnswer } from "../utils/position";
 
 const networkConnections = [
-    createNetworkConnections("Yael davidov", "Software Developer", "https://www.linkedin.com/in/yael-davidov/", true),
-    createNetworkConnections("Eldar Sehayek", "Senior Software Architect", "https://www.linkedin.com/in/eldar-sehayek/", false),
-    createNetworkConnections("Matan Peretz", "Software Developer", "https://www.linkedin.com/in/matan-peretz/", false)
+    new NetworkConnection("Yael davidov", "Software Developer", "https://www.linkedin.com/in/yael-davidov/", true),
+    new NetworkConnection("Eldar Sehayek", "Senior Software Architect", "https://www.linkedin.com/in/eldar-sehayek/", false),
+    new NetworkConnection("Matan Peretz", "Software Developer", "https://www.linkedin.com/in/matan-peretz/", false)
 ]
-
-function createFile(path, name){
-    return new File([path], name);
-}
-
-export function incrementHomeAssignmentsId(){
-    return homeAssignmentsId++;
-}
 
 const homeAssignments = [
-    createFile(["../testData/files/file1.docx"], "file1.docx"),
-    createFile(["../testData/files/file2.docx"], "file2.docx"),
-    createFile(["../testData/files/file3.pdf"], "file3.pdf")
+    new HomeAssignment(["../testData/files/file1.docx"], "file1.docx"),
+    new HomeAssignment(["../testData/files/file2.docx"], "file2.docx"),
+    new HomeAssignment(["../testData/files/file3.pdf"], "file3.pdf")
 ];
-
-export function incrementPositionQuestionId(){
-    return positionQuestionsId++;
-}
-
-function createQuestionsForInterviewers(question, answer){
-    const id = positionQuestionsId++;
-    return { question, answer, id };
-}
 
 const questionsAndAnswers = [
-    createQuestionsForInterviewers("What is the tech stack?", "Java, MySQL, JS, React"),
-    createQuestionsForInterviewers("What is the tech stack?", "Java, MySQL, JS, React"),
-    createQuestionsForInterviewers("How many steps are there in the process?", ""),
+    new QuestionsAndAnswer("What is the tech stack?", "Java, MySQL, JS, React"),
+    new QuestionsAndAnswer("What is the tech stack?", "Java, MySQL, JS, React"),
+    new QuestionsAndAnswer("How many steps are there in the process?", ""),
 ]
 
-export function incrementInterviewsId(){
-    return interviewId++;
-}
-
-function createInterview(date, title, conclusions, file){
-    const id = interviewId++;
-    return { date, title, conclusions, file, id };
-}
-
 const interviews = [
-    createInterview(new Date(2023, 1, 15).toLocaleDateString(), "HR interview", "need to practice more"),
-    createInterview(new Date(2023, 1, 17).toLocaleDateString(), "Tech interview no.1", "need to do more leetcode questions"),
-    createInterview(new Date(2023, 1, 30).toLocaleDateString(), "Tech interview no.2", "need to do more leetcode questions"),
-    createInterview(new Date(2023, 2, 15).toLocaleDateString(), "CTO interview", "need to do more leetcode questions"),
-    // createInterview(new Date(), "HR interview", "need to practice more"),
-    // createInterview(new Date(), "Tech interview no.1", "need to do more leetcode questions"),
-    // createInterview(new Date(), "Tech interview no.2", "need to do more leetcode questions"),
-    // createInterview(new Date(), "CTO interview", "need to do more leetcode questions"),
-
+    new Interview(new Date(2023, 1, 15).toLocaleDateString(), "HR interview", "need to practice more"),
+    new Interview(new Date(2023, 1, 17).toLocaleDateString(), "Tech interview no.1", "need to do more leetcode questions"),
+    new Interview(new Date(2023, 1, 30).toLocaleDateString(), "Tech interview no.2", "need to do more leetcode questions"),
+    new Interview(new Date(2023, 2, 15).toLocaleDateString(), "CTO interview", "need to do more leetcode questions"),
 ];
 
-function createPositionInfo(companyName, positionLink, role, connections, status, about, techStack){
-    return {companyName, positionLink, role, connections, status, about, techStack}
-}
-
-const positionInfo =  createPositionInfo("Google", "lknfnw", "fullStack", 5, "not applied", "something", "C#, MySQL, js...");
-    // createPositionInfo("Microsoft", "lknfnw", "fullStack", 5, "not applied", "something", "C#, MySQL, js..."),
-    // createPositionInfo("Wix", "lknfnw", "fullStack", 5, "not applied", "something", "C#, MySQL, js..."),
-    // createPositionInfo("Facebook", "lknfnw", "fullStack", 5, "not applied", "something", "C#, MySQL, js..."),
-    // createPositionInfo("Intel", "lknfnw", "fullStack", 5, "not applied", "something", "C#, MySQL, js..."),
-
-
-function createPosition(positionInfo, interviews, questionsAndAnswers, homeAssignments, networkConnections){
-    const id = positionId++;
-    return {id ,positionInfo, interviews, questionsAndAnswers, homeAssignments, networkConnections};
-}
+const positionInfo =  new PositionInfo("Google", "lknfnw", "fullStack", 5, "not applied", "something", "C#, MySQL, js...");
 
 const positions = [
-    createPosition(positionInfo, interviews, questionsAndAnswers, homeAssignments, networkConnections)
+    new Position(positionInfo, interviews, questionsAndAnswers, homeAssignments, networkConnections)
 ];
 
 export default positions;
