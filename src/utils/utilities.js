b7b3b97d0664fd7c787167701b25190f049d4ca5
Yael Davidov <yael.davidov97@gmail.com>
2023-07-24 11:45:47 +0300

    Create dashboard page
    - Create dashboard page
    - Add dashboard page as home route in index.js file.
    - Transfer the function "filterAndSortPositions" from the JobsTable component to utils/utilities file.
    - Add the dashboard page to the SideBar component.
    - Get all positions using the hook "useGetAllPositions" in Jobs page component.

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,5 +1,5 @@
-export function handleDownloadFile(file){
-    try{
+export function handleDownloadFile(file) {
+    try {
         //create a temporary URL for the file
         const fileURL = URL.createObjectURL(file);
 
@@ -12,7 +12,33 @@ export function handleDownloadFile(file){
         //clean up the temporary URL
         URL.revokeObjectURL(fileURL);
     }
-    catch(error){
+    catch (error) {
         console.error('Error downloading file:', error);
     }
+}
+
+export function createAllTodos(positions) {
+    const res = [];
+    positions.forEach((position) => {
+        res.push(...position.todoList);
+    });
+    return res;
+}
+
+export function filterAndSortPositions(positions, status, order) {
+    let filteredPositions = positions.slice();
+    if (status !== "All positions") {
+        filteredPositions = filteredPositions.filter((currPosition) => {
+            return currPosition.positionInfo.status === status
+        });
+    }
+    const sortedPositions = filteredPositions.sort((obj1, obj2) => {
+        if (order === 'desc') {
+            return obj2.positionInfo.date - obj1.positionInfo.date;
+        }
+        else {
+            return obj1.positionInfo.date - obj2.positionInfo.date;
+        }
+    });
+    return sortedPositions;
 }
